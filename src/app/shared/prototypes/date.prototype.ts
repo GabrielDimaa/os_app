444592cd749c2246ac2e5e42f35e1c401b9358c6
Gabriel de Matos Hainzenreder <gabrielmatos1999@rede.ulbra.br>
2023-01-30 20:56:05 -0300

    dateWithoutTimezone

--- src/app/shared/prototypes/date.prototype.ts
+++ src/app/shared/prototypes/date.prototype.ts
@@ -4,12 +4,16 @@ declare global {
   }
 }
 
-Date.prototype.toJSONLocal = function() {
+Date.prototype.toJSONLocal = function () {
   const hoursDiff = this.getHours() - this.getTimezoneOffset() / 60;
   this.setHours(hoursDiff);
 
-  const d = this.toISOString().replace("Z", "").split(".");;
+  const d = this.toISOString().replace("Z", "").split(".");
   return `${d[0]}.${d[1].padEnd(6, "0")}`;
 };
 
-export {}
+function dateWithoutTimezone(dateString: string): Date {
+  return new Date(dateString.replace("Z", ""));
+}
+
+export { dateWithoutTimezone }
