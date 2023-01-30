29c0eedd8b0b0a1d1ac92b8e81e800b91fd8edbd
Gabriel de Matos Hainzenreder <gabrielmatos1999@rede.ulbra.br>
2023-01-30 20:54:55 -0300

    OsServicoModel

--- src/app/os/models/os-servico.model.ts
+++ src/app/os/models/os-servico.model.ts
@@ -1,17 +1,33 @@
 import { UsuarioAPI, UsuarioModel } from "./usuario.model";
 import { ServicoAPI, ServicoModel } from "./servico.model";
+import { dateWithoutTimezone } from "../../shared/prototypes/date.prototype";
 
 class OsServicoModel {
   id: number;
   idOsEquipamentoItem: number;
   qtd: string;
+  valorTotal: number | null;
+  descricaoInformada: string | null;
+  dataHora: Date | null;
   servico: ServicoModel;
   usuario: UsuarioModel | null;
 
-  constructor(id: number, idOsEquipamentoItem: number, qtd: string, servico: ServicoModel, usuario: UsuarioModel | null) {
+  constructor(
+    id: number,
+    idOsEquipamentoItem: number,
+    qtd: string,
+    valorTotal: number | null,
+    descricaoInformada: string | null,
+    dataHora: Date | null,
+    servico: ServicoModel,
+    usuario: UsuarioModel | null
+  ) {
     this.id = id;
     this.idOsEquipamentoItem = idOsEquipamentoItem;
     this.qtd = qtd;
+    this.valorTotal = valorTotal;
+    this.descricaoInformada = descricaoInformada;
+    this.dataHora = dataHora;
     this.servico = servico;
     this.usuario = usuario;
   }
@@ -21,6 +37,9 @@ class OsServicoModel {
       json.id_os_servico,
       json.id_os_equipamento_item,
       json.qtd,
+      json.valor_total,
+      json.descricao_informada,
+      json.data_hora ? dateWithoutTimezone(json.data_hora) : null,
       ServicoModel.fromJson(json.servico),
       json.usuario != null ? UsuarioModel.fromJson(json.usuario) : null
     );
@@ -31,6 +50,9 @@ interface OsServicoAPI {
   id_os_servico: number;
   id_os_equipamento_item: number;
   qtd: string;
+  valor_total: number | null,
+  descricao_informada: string | null,
+  data_hora: string | null,
   servico: ServicoAPI,
   usuario: UsuarioAPI | null;
 }
