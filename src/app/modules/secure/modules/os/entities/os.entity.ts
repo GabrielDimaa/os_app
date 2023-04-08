f04a8fec05d6e911ed572bc8d4c907b922192bbe
Gabriel de Matos <gabrielmatos1999@rede.ulbra.br>
2023-04-08 16:19:04 -0300

    Tratado quebras de linha

--- src/app/modules/secure/modules/os/entities/os.entity.ts
+++ src/app/modules/secure/modules/os/entities/os.entity.ts
@@ -89,7 +89,7 @@ export default class OsEntity {
     return {
       id_os: this.id,
       os_codigo: this.codigo,
-      obs: this.obs,
+      obs: this.obs?.replaceAll("\n", "\r\n") ?? "",
       inativo: this.inativo,
       id_os_tipo_atendimento: this.tipoAtendimento!.id,
       tipo_atendimento: this.tipoAtendimento!.toModel(),
